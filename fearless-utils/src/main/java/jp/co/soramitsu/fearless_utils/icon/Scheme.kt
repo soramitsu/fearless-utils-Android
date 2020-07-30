c749ce4ea6a3a0961d04873cd75e1e228853c9c7
Ali Abdulmadzhidov <mrfreezzzer@mail.ru>
2020-07-30 13:07:08 +0300

    pr fix
    Signed-off-by: Ali Abdulmadzhidov <mrfreezzzer@mail.ru>

--- fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/icon/Scheme.kt
+++ fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/icon/Scheme.kt
@@ -1,3 +1,7 @@
 package jp.co.soramitsu.fearless_utils.icon
 
-data class Scheme(val name: String, val frequency: Int, val colors: Array<Int>)
+data class Scheme(
+    val name: String,
+    val frequency: Int,
+    val colors: Array<Int>
+)
--- fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils_android/ss58/AddressType.kt
+++
@@ -1,7 +0,0 @@
-package jp.co.soramitsu.fearless_utils_android.ss58
-
-enum class AddressType(val addressByte: Byte) {
-    WESTEND(42.toByte()),
-    KUSAMA(2.toByte()),
-    POLKADOT(0.toByte())
-}
