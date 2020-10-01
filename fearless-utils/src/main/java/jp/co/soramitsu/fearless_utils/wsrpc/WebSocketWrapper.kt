a5d92fc5242eca953b9e196a7cc2f60a8dbdea0e
Valentun <karbon4ic@gmail.com>
2020-10-01 13:33:02 +0300

    websocket logger

--- fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/wsrpc/WebSocketWrapper.kt
+++ fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/wsrpc/WebSocketWrapper.kt
@@ -1,17 +1,24 @@
 package jp.co.soramitsu.fearless_utils.wsrpc
 
 import com.google.gson.Gson
-import com.neovisionaries.ws.client.WebSocketFactory
-import com.neovisionaries.ws.client.WebSocketAdapter
 import com.neovisionaries.ws.client.WebSocket
+import com.neovisionaries.ws.client.WebSocketAdapter
 import com.neovisionaries.ws.client.WebSocketException
+import com.neovisionaries.ws.client.WebSocketFactory
 import jp.co.soramitsu.fearless_utils.wsrpc.request.base.RpcRequest
 import jp.co.soramitsu.fearless_utils.wsrpc.response.RpcResponse
+
+interface Logger {
+    fun log(message: String?)
+
+    fun log(throwable: Throwable?)
+}
 
 class WebSocketWrapper(
     url: String,
     listener: WebSocketResponseListener,
-    val singleResponse: Boolean = true
+    val singleResponse: Boolean = true,
+    val logger: Logger? = null
 ) {
 
     private val ws = WebSocketFactory().createSocket(url)
@@ -20,7 +27,7 @@ class WebSocketWrapper(
     init {
         ws.addListener(object : WebSocketAdapter() {
             override fun onTextMessage(websocket: WebSocket?, text: String?) {
-                super.onTextMessage(websocket, text)
+                logger?.log("[RECEIVED] $text")
 
                 listener.onResponse(gson.fromJson(text, RpcResponse::class.java))
 
@@ -30,7 +37,8 @@ class WebSocketWrapper(
             }
 
             override fun onError(websocket: WebSocket?, cause: WebSocketException?) {
-                super.onError(websocket, cause)
+                logger?.log(cause)
+
                 listener.onError(cause!!)
             }
         })
@@ -50,6 +58,9 @@ class WebSocketWrapper(
 
     fun sendRpcRequest(rpcRequest: RpcRequest) {
         val text = gson.toJson(rpcRequest)
+
+        logger?.log("[SENDING] $text")
+
         ws.sendText(text)
     }
 }
