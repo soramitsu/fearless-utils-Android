1d7ed9aa53def4dc72db9b969cfe868f794b627d
Valentun <karbon4ic@gmail.com>
2020-10-19 08:55:54 +0300

    [Fix] digest not visible for consumers

--- fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/hash/Blake2b32.kt
+++ fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/hash/Blake2b32.kt
@@ -6,6 +6,15 @@ import org.bouncycastle.jcajce.provider.digest.BCMessageDigest
 class Blake2b32 : BCMessageDigest(Blake2bDigest(32)),
     Cloneable {
 
+    /**
+     * Parent method is hidden from consumers because implementation directive is used,
+     * meaning there is no transitive visibility
+     */
+    @Suppress("RedundantOverride")
+    override fun digest(input: ByteArray): ByteArray {
+        return super.digest(input)
+    }
+
     override fun clone(): Any {
         val d = super<BCMessageDigest>.clone() as Blake2b32
 
