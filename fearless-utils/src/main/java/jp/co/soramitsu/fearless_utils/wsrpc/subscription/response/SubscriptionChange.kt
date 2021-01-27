752de15e1a9973a9cd1193ec1fe9c194d2631241
Arvi Fox <arvifox@hotmail.com>
2021-01-27 17:36:36 +0300

    subscription any result

--- fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/wsrpc/subscription/response/SubscriptionChange.kt
+++ fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/wsrpc/subscription/response/SubscriptionChange.kt
@@ -1,20 +1,12 @@
 package jp.co.soramitsu.fearless_utils.wsrpc.subscription.response
 
-data class Change(val value: String?)
-
 class SubscriptionChange(
     val jsonrpc: String,
     val method: String,
     val params: Params
 ) {
 
-    class Params(val result: Result, val subscription: String) {
-
-        // changes are in format [[storage key, value], [..], ..]
-        class Result(val block: String, val changes: List<List<String?>>) {
-            fun getSingleChange() = Change(changes.first()[1])
-        }
-    }
+    class Params(val result: Any, val subscription: String)
 
     val subscriptionId: String
         get() = params.subscription
