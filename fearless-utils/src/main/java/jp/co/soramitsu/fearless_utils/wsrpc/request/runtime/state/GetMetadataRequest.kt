5833dbf4adcc95d4ad8633d049e551343ad69e0a
Valentun <sergeev@soramitsu.co.jp>
2021-02-03 13:46:59 +0300

    Refactoring

--- fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/wsrpc/request/runtime/state/GetMetadataRequest.kt
+++
@@ -1,5 +0,0 @@
-package jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.state
-
-import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.RuntimeRequest
-
-object GetMetadataRequest : RuntimeRequest("state_getMetadata", listOf())
