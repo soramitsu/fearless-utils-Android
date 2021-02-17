608b2d88d9c5ebfae564a82966cd9d866727a966
Valentun <sergeev@soramitsu.co.jp>
2021-02-17 16:52:08 +0300

    Fluent interface style for extrinsic builder

--- fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/runtime/extrinsic/ExtrinsicBuilderExt.kt
+++ fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/runtime/extrinsic/ExtrinsicBuilderExt.kt
@@ -3,8 +3,8 @@ package jp.co.soramitsu.fearless_utils.runtime.extrinsic
 import jp.co.soramitsu.fearless_utils.runtime.definitions.types.composite.DictEnum
 import java.math.BigInteger
 
-fun ExtrinsicBuilder.transfer(recipientAccountId: ByteArray, amount: BigInteger) {
-    call(
+fun ExtrinsicBuilder.transfer(recipientAccountId: ByteArray, amount: BigInteger): ExtrinsicBuilder {
+    return call(
         moduleName = "Balances",
         callName = "transfer",
         arguments = mapOf(
