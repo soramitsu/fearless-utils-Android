cb0ed263cae57404a2ed6bd2aa3fc6a19afe7ceb
Arvi Fox <arvifox@hotmail.com>
2021-04-13 10:13:34 +0300

    Feature/ss58prefix (#85)
    * ss58 prefix
    
    * ss58 prefix
    
    * ss58 prefix

--- fearless-utils/src/test/java/jp/co/soramitsu/fearless_utils/ss58/SS58EncoderTest.kt
+++ fearless-utils/src/test/java/jp/co/soramitsu/fearless_utils/ss58/SS58EncoderTest.kt
@@ -3,10 +3,10 @@ package jp.co.soramitsu.fearless_utils.ss58
 import jp.co.soramitsu.fearless_utils.common.TestAddressBytes
 import jp.co.soramitsu.fearless_utils.extensions.fromHex
 import jp.co.soramitsu.fearless_utils.extensions.toHexString
+import jp.co.soramitsu.fearless_utils.ss58.SS58Encoder.addressByte
 import jp.co.soramitsu.fearless_utils.ss58.SS58Encoder.toAccountId
 import jp.co.soramitsu.fearless_utils.ss58.SS58Encoder.toAddress
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
@@ -29,5 +29,69 @@ class SS58EncoderTest {
         val result = KUSAMA_ADDRESS.toAccountId().toHexString()
 
         assertEquals(PUBLIC_KEY, result)
+    }
+
+    @Test
+    fun `encode key to address with 69 prefix`() {
+        val hexKey = "0x84bdc405d139399bba3ccea5d3de23316c9deeab661f57e2f4d1720cc6649859"
+        val address = hexKey.fromHex().toAddress(69)
+        assertEquals("cnUVLAjzRsrXrzEiqjxMpBwvb6YgdBy8DKibonvZgtcQY5ZKe", address)
+    }
+
+    @Test
+    fun `encode key to address with kusama prefix`() {
+        val hexKey = "0x84bdc405d139399bba3ccea5d3de23316c9deeab661f57e2f4d1720cc6649859"
+        val address = hexKey.fromHex().toAddress(TestAddressBytes.KUSAMA)
+        assertEquals("FaNDBF8erbEfQtpLuwpk6kmQrYyiKNzBQm6BSuh2fdCqajb", address)
+    }
+
+    @Test
+    fun `encode key to address with polkadot prefix`() {
+        val hexKey = "0x84bdc405d139399bba3ccea5d3de23316c9deeab661f57e2f4d1720cc6649859"
+        val address = hexKey.fromHex().toAddress(TestAddressBytes.POLKADOT)
+        assertEquals("1413hCAKtGqnMJ5tXrBmzJDv7tGPbx7woXepx5d66xSEH6qM", address)
+    }
+
+    @Test
+    fun `encode key to address with westend prefix`() {
+        val hexKey = "0x84bdc405d139399bba3ccea5d3de23316c9deeab661f57e2f4d1720cc6649859"
+        val address = hexKey.fromHex().toAddress(TestAddressBytes.WESTEND)
+        assertEquals("5F4kYruG2VaJum5NaD8mr9PmGGGjueZoj2vLnndjYsQi6Vkw", address)
+    }
+
+    @Test
+    fun `decode address to key with 69 prefix`() {
+        val address = "cnUVLAjzRsrXrzEiqjxMpBwvb6YgdBy8DKibonvZgtcQY5ZKe"
+        val hexKey = address.toAccountId().toHexString(true)
+        val prefix = address.addressByte()
+        assertEquals("0x84bdc405d139399bba3ccea5d3de23316c9deeab661f57e2f4d1720cc6649859", hexKey)
+        assertEquals(69.toByte(), prefix)
+    }
+
+    @Test
+    fun `decode address to key with kusama prefix`() {
+        val address = "FaNDBF8erbEfQtpLuwpk6kmQrYyiKNzBQm6BSuh2fdCqajb"
+        val hexKey = address.toAccountId().toHexString(true)
+        val prefix = address.addressByte()
+        assertEquals("0x84bdc405d139399bba3ccea5d3de23316c9deeab661f57e2f4d1720cc6649859", hexKey)
+        assertEquals(TestAddressBytes.KUSAMA, prefix)
+    }
+
+    @Test
+    fun `decode address to key with polkadot prefix`() {
+        val address = "1413hCAKtGqnMJ5tXrBmzJDv7tGPbx7woXepx5d66xSEH6qM"
+        val hexKey = address.toAccountId().toHexString(true)
+        val prefix = address.addressByte()
+        assertEquals("0x84bdc405d139399bba3ccea5d3de23316c9deeab661f57e2f4d1720cc6649859", hexKey)
+        assertEquals(TestAddressBytes.POLKADOT, prefix)
+    }
+
+    @Test
+    fun `decode address to key with westend prefix`() {
+        val address = "5F4kYruG2VaJum5NaD8mr9PmGGGjueZoj2vLnndjYsQi6Vkw"
+        val hexKey = address.toAccountId().toHexString(true)
+        val prefix = address.addressByte()
+        assertEquals("0x84bdc405d139399bba3ccea5d3de23316c9deeab661f57e2f4d1720cc6649859", hexKey)
+        assertEquals(TestAddressBytes.WESTEND, prefix)
     }
 }
