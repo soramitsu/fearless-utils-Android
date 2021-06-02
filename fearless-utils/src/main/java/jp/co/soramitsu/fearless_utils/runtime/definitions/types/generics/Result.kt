bdb89efa136498e51c0c67c231167620af741c9c
arvifox <arvifox@hotmail.com>
2021-06-02 19:40:27 +0300

    substrate result type

--- fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/runtime/definitions/types/generics/Result.kt
+++ fearless-utils/src/main/java/jp/co/soramitsu/fearless_utils/runtime/definitions/types/generics/Result.kt
@@ -1,12 +1,7 @@
 package jp.co.soramitsu.fearless_utils.runtime.definitions.types.generics
 
-import io.emeraldpay.polkaj.scale.ScaleCodecReader
-import io.emeraldpay.polkaj.scale.ScaleCodecWriter
-import jp.co.soramitsu.fearless_utils.runtime.RuntimeSnapshot
 import jp.co.soramitsu.fearless_utils.runtime.definitions.types.TypeReference
 import jp.co.soramitsu.fearless_utils.runtime.definitions.types.composite.DictEnum
-import jp.co.soramitsu.fearless_utils.runtime.definitions.types.errors.EncodeDecodeException
-import jp.co.soramitsu.fearless_utils.scale.dataType.byte
 
 class ResultType(ok: TypeReference, err: TypeReference) : DictEnum(
     "Result", listOf(
@@ -18,43 +13,5 @@ class ResultType(ok: TypeReference, err: TypeReference) : DictEnum(
     companion object {
         const val Ok = "Ok"
         const val Err = "Err"
-    }
-
-    override fun decode(scaleCodecReader: ScaleCodecReader, runtime: RuntimeSnapshot): Entry<Any?> {
-
-        return when (val typeIndex = byte.read(scaleCodecReader).toInt()) {
-            0 -> {
-                val instance = elements[0].value.requireValue().decode(scaleCodecReader, runtime)
-                Entry(Ok, instance)
-            }
-
-            1 -> {
-                val instance = elements[1].value.requireValue().decode(scaleCodecReader, runtime)
-                Entry(Err, instance)
-            }
-
-            else -> throw EncodeDecodeException("Cannot process $typeIndex for Result type")
-        }
-    }
-
-    override fun encode(
-        scaleCodecWriter: ScaleCodecWriter,
-        runtime: RuntimeSnapshot,
-        value: Entry<Any?>
-    ) {
-        when (value.name) {
-            Ok -> {
-                val index = 0
-                byte.write(scaleCodecWriter, index.toByte())
-                elements[0].value.requireValue()
-                    .encodeUnsafe(scaleCodecWriter, runtime, value.value)
-            }
-            else -> {
-                val index = 1
-                byte.write(scaleCodecWriter, index.toByte())
-                elements[1].value.requireValue()
-                    .encodeUnsafe(scaleCodecWriter, runtime, value.value)
-            }
-        }
     }
 }
